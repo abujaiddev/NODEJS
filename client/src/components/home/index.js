3a6042913bb1396e797505d6b7f346d61db62358
abujaiddev <abu.jaid@infogain.com>
2020-03-15 22:25:33 +0530

    change home page desing

---
+++ client/src/components/home/index.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import './user.css'
+import { MdPeople } from "react-icons/md";
+
+export default class Home extends React.Component {
+  render() {
+    return (
+
+      <div className="container-fluid mt-3">
+        <div className="row">
+          <div className="col-sm-3 text-center">
+
+            <div className="row">
+              <div className="col-sm-3 bg-danger">col-sm-2</div>
+              <div className="col-sm-9">
+                <ul class="nav flex-column float-left">
+                  <li class="nav-item">
+                    <a class="nav-link active" href="#">Active</a>
+                  </li>
+                  <li class="nav-item ls">
+                    <MdPeople /><a class="nav-link" href="#">Link</a>
+                  </li>
+                  <li class="nav-item ls">
+                    <a class="nav-link" href="#">Link</a>
+                  </li>
+                  <li class="nav-item ls">
+                    <a class="nav-link disabled" href="#">Disabled</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-7">
+            <div className="row">
+              <div className="col-sm-8">
+                <div className="card">
+                  <div className="card-header">Create Post</div><div className="card-body">
+                    <textarea name="" className="form-control" rows="2"></textarea>
+                  </div>
+                </div>
+                <div className="card mt-3"><div className="card-body">
+                  <span className="">Stories</span>
+                  <a href="#" className="float-right">See All</a>
+                </div></div>
+                <div className="card mt-3"><div className="card-body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eaque ut facere, numquam illum quibusdam repellendus, maiores modi laboriosam minima rem voluptatum fugit impedit eveniet excepturi id in doloremque eius.</div></div>
+              </div>
+              <div className="col-sm-4"><div className="card"><div className="card-body">Abu jaid</div></div>
+                <div className="card mt-3"><div className="card-body">Your pages</div></div>
+                <div className="card mt-3"><div className="card-body">Listing You Recently Viewed</div></div>
+                <div className="card mt-3"><div className="card-body">Sponsored</div></div>
+                <div className="card mt-3"><div className="card-body">
+                  <div className="languages"><a href="#">English (US)</a>{" "}
+                    <a href="#">हिन्दी</a>{" "}
+                    <a href="#">اردو</a>{" "}
+                    <a href="#">ਪੰਜਾਬੀ</a>{" "}
+                    <a href="#">தமிழ்</a>{" "}
+                    <a className="btn btn-light border">+</a></div>
+                </div></div>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-2 bg-danger">col-sm-1</div>
+        </div></div>
+    )
+  }
+}
--- client/src/components/routes/routes.js
+++ client/src/components/routes/routes.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 
 import Login from "../auth/login";
-import User from "../user";
+import Home from "../home";
 import UserRoute from "./UserRoute";
 import Signup from "../auth/signup";
 import Profile from "../dashboard/user/profile";
@@ -10,14 +10,14 @@ import Users from "../users/users";
 import Settings from "../settings";
 import UserProfile from "../profile";
 import FindFriends from "../users/findFriends";
-import Userdashboard from "../user/user";
+import Userdashboard from "../home/user";
 
 export default function routes() {
   return (
     <Switch>
       <Route path="/login" component={Login} />
       <Route path="/signup" component={Signup} />
-      <UserRoute exact path="/" component={User} />
+      <UserRoute exact path="/" component={Home} />
       <UserRoute exact path="/user/profile" component={Profile} />
       <UserRoute exact path="/users" component={Users} />
       <UserRoute exact path="/settings" component={Settings} />
